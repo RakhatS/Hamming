3a1e960de6d10fb54e24431530bdfcda4158d833
Rakhat <91605148+RakhatS@users.noreply.github.com>
2022-11-30 10:47:35 +0600

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,12 +5,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int powerOfTwo[] = {1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,65536};
-        int lengthOfHammingCode = 0;
+        int powerOfTwo[] = {1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,65536}, lengthCode = 0;
         Scanner in = new Scanner(System.in);
-        System.out.print("Кодтағыңыз келетін екілік санды енгізіңіз: "); String code = in.nextLine();
+        System.out.print("Кодтағыңыз келетін екілік санды енгізіңіз: "); 
+        String code = in.nextLine();
         in.close();
-        if (code.length() < 2) System.out.println("Өте аз сан.");
+        if (code.length() < 2) 
+            System.out.println("Өте аз сан.");
 
         boolean controlBits[] = new boolean[code.length()*2+1];
         int i = 0;
@@ -21,13 +22,13 @@ public class Main {
             i++;
         } while (powerOfTwo[i] <= (code.length()+1)); // Цикл екінің ЕҢ ТӨМЕН қуатын іздейді, ол > немесе = кіріс кодының ұзындығы.
 
-        lengthOfHammingCode = code.length() + i;
+        lengthCode = code.length() + i;
         //----------------------------------------------------------------------
 
         //-------------ЕНГІЗІЛГЕН КОДТЫ БАСҚАРУ БИТТЕРІМЕН ЖАЗЫҢЫЗ-----------
-        boolean hammingCode[] = new boolean[lengthOfHammingCode];
+        boolean hammingCode[] = new boolean[lengthCode];
         int j = 0;
-        for (i = 0; i < lengthOfHammingCode; i++) { // Устанавливаем биты для промежуточного кода.
+        for (i = 0; i < lengthCode; i++) { // 
             if (!controlBits[i+1]) {
                 hammingCode[i] = (code.charAt(j) == '1');
                 j++;
@@ -36,28 +37,28 @@ public class Main {
         //----------------------------------------------------------------------
 
         System.out.print("Басқару биттері бар аралық код: [");
-        for (int k = 0; k < lengthOfHammingCode; k++) {
+        for (int k = 0; k < lengthCode; k++) {
             char c = (hammingCode[k]) ? '1' : '0';
             if (controlBits[k+1]) System.out.print("!");
             System.out.print(c);
         }
         System.out.print("]\n");
 
-        //---------------------БАСҚАРУ БИТТЕРІНІҢ МӘНІН ЕСЕПТЕҢІЗ----------------
-        for (i = 0; i < (lengthOfHammingCode - code.length()); i++) { // Барлық басқару биттерін айналдырыңыз.
+        //---------------------БАСҚАРУ БИТТЕРІНІҢ МӘНІН ЕСЕПТЕУ----------------
+        for (i = 0; i < (lengthCode - code.length()); i++) { // Аралық код үшін биттерді орнатамыз
             boolean nextBit = false;
-            for (j = powerOfTwo[i]-1; j < lengthOfHammingCode; j += powerOfTwo[i]*2) {
+            for (j = powerOfTwo[i]-1; j < lengthCode; j += powerOfTwo[i]*2) {
                 for (int n = j; n < j + powerOfTwo[i]; n++) {
-                    if (n > lengthOfHammingCode - 1) break;
+                    if (n > lengthCode - 1) break;
                     nextBit ^= hammingCode[n];
                 }
             }
             hammingCode[powerOfTwo[i]-1] = nextBit; // i-биттің разрядтың мәнін жазамыз.
         }
         //----------------------------------------------------------------------
-
+        
         System.out.print("Басқару биттері бар Хамминг коды: [");
-        for (int k = 0; k < lengthOfHammingCode; k++) {
+        for (int k = 0; k < lengthCode; k++) {
             char c = (hammingCode[k]) ? '1' : '0';
             System.out.print(c);
         }
